e122fc72f59317513ce9cfa6f5b3db042bb5c900
Bulgun <bulgunya@mail.ru>
2022-11-23 01:13:05 +0300

    add lesson3

---
+++ lesson3_Methods/Program.cs
@@ -0,0 +1,75 @@
+﻿// 1 группа
+
+// void Method1()
+// {
+//     Console.WriteLine("Автор ...");
+// }
+// Method1();
+
+// 2 группа
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg: "Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(msg: "Текст", count: 4);
+// Method21(count: 4, msg: "Новый текст");
+
+// 3 группа
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+// 4 группа
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty; // означает пустая строка, можно и так: "";
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// цикл for
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "Z");
+//Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
---
+++ lesson3_tasks/Program.cs
@@ -0,0 +1,118 @@
+﻿// Работа с текстом
+// дан текст. нужно все пробелы заменить черточками
+// маленькие буквы "к" заменить большими "К"
+// большие "С" заменить маленькими "с"
+
+// string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля."
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// // string s = "qwerty"
+// //             012345
+// // s[3] = r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(text, 'к', 'К');
+// Console.WriteLine(newText);
+
+
+// сортировка массива от меньшего числа к большему
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length; // количество элементов массива
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+
+// }
+
+// void SelectionSort(int[] array)
+// {
+
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+
+
+//         int temporary = array[i]; // временная позиция
+//         array[i] = array[minPosition]; // замена позиции: в текущую кладут минимальную
+//         array[minPosition] = temporary;
+
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
+
+
+
+// сортировка от большего к меньшему
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length; // количество элементов массива
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+
+}
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+
+        int temporary = array[i]; // временная позиция
+        array[i] = array[maxPosition]; // замена позиции: в текущую кладут максимальную
+        array[maxPosition] = temporary;
+
+    }
+}
+
+PrintArray(arr);
+
+SelectionSort(arr);
+PrintArray(arr);
